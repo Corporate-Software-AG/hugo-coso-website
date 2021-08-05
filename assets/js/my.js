cc967623adb1293b92bd1d7039916d5407cdb042
Thomas <thomas@vonmentlen.info>
2021-08-05 15:23:43 +0200

    add preheader on main page

--- assets/js/my.js
+++ assets/js/my.js
@@ -2,8 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
     setCopyright();
 }, false);
 
-
-
 function setCopyright() {
     document.getElementById("copyright").innerText = "© " + new Date().getFullYear() + " Corporate Software AG";
+}
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {toggleStickyClass()};
+
+// Get the header
+var header = document.getElementById("coso-header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function toggleStickyClass() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
