0e76416c5a08f01158b9de5b2796fe5cbe07efd1
Thomas <65050898+tvonment@users.noreply.github.com>
2021-09-14 12:51:28 +0000

    try

--- script/translate.py
+++ script/translate.py
@@ -2,28 +2,43 @@ import os
 import requests
 
 print("Start Translating")
+
+"""
 print(os.environ['key'])
 print(os.environ['location'])
 print(os.environ['name'])
-
 
 endpoint = "https://" + os.environ['name'] + ".cognitiveservices.azure.com/translator/text/batch/v1.0"
 subscriptionKey =  os.environ['key']
 path = '/batches'
 constructed_url = endpoint + path
+"""
+
+endpoint = "https://coso-web-translator.cognitiveservices.azure.com/translator/text/batch/v1.0"
+subscriptionKey =  "e392619116934fd494c05a14e2d0d5e6"
+path = '/batches'
+constructed_url = endpoint + path
+
 
 print(constructed_url)
 
-payload = {
+payload= {
     "inputs": [
         {
             "source": {
-                "sourceUrl": "https://cosoweb.blob.core.windows.net/translation/de?sv=2020-08-04&ss=b&srt=sco&sp=rwdlacx&se=2021-09-14T19:57:34Z&st=2021-09-14T11:57:34Z&spr=https&sig=hw%2FEDxJoKgTdxlKrbikov%2BinXlzk6Lc6%2BNGz360540Y%3D"
+                "sourceUrl": "https://cosoweb.blob.core.windows.net/source/homepage.yml?sv=2020-08-04&ss=b&srt=c&sp=rwdlacx&se=2021-09-14T20:34:15Z&st=2021-09-14T12:34:15Z&spr=https&sig=bgMYQ3A86wSEt248JzB5eTl6wOCO0M%2FvuEcS0cn9Iqk%3D",
+                "storageSource": "AzureBlob",
+                "language": "en",
+                "filter":{
+                    "prefix": "Demo_1/"
+                }
             },
             "targets": [
                 {
-                    "targetUrl": "https://cosoweb.blob.core.windows.net/translation/en?sv=2020-08-04&ss=b&srt=sco&sp=rwdlacx&se=2021-09-14T19:57:34Z&st=2021-09-14T11:57:34Z&spr=https&sig=hw%2FEDxJoKgTdxlKrbikov%2BinXlzk6Lc6%2BNGz360540Y%3D",
-                    "language": "en"
+                    "targetUrl": "https://cosoweb.blob.core.windows.net/destination/homepage-es.yml?sv=2020-08-04&ss=b&srt=c&sp=rwdlacx&se=2021-09-14T20:34:15Z&st=2021-09-14T12:34:15Z&spr=https&sig=bgMYQ3A86wSEt248JzB5eTl6wOCO0M%2FvuEcS0cn9Iqk%3D",
+                    "storageSource": "AzureBlob",
+                    "category": "general",
+                    "language": "es"
                 }
             ]
         }
