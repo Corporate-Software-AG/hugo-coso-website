912aa72046413892dd79b69586f66e3974fb0dd1
Thomas <thomas@vonmentlen.info>
2021-09-14 13:32:16 +0200

    add env variables

--- script/translate.py
+++ script/translate.py
@@ -1,1 +1,14 @@
+import os
+
 print("Start Translating")
+print(os.environ['key'])
+print(os.environ['location'])
+print(os.environ['name'])
+
+
+endpoint = "https://" + os.environ['name'] + ".cognitiveservices.azure.com/translator/text/batch/v1.0"
+subscriptionKey =  os.environ['key']
+path = '/batches'
+constructed_url = endpoint + path
+
+print(constructed_url)
