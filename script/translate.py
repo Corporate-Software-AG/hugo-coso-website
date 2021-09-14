934855acd9ac28793e74f28c2250405b297e3063
Thomas <thomas@vonmentlen.info>
2021-09-14 13:36:32 +0200

    start

--- script/translate.py
+++ script/translate.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 print("Start Translating")
 print(os.environ['key'])
@@ -12,3 +13,28 @@ path = '/batches'
 constructed_url = endpoint + path
 
 print(constructed_url)
+
+{
+    "inputs": [
+        {
+            "source": {
+                "sourceUrl": "data/de"
+            },
+            "targets": [
+                {
+                    "targetUrl": "data/en",
+                    "language": "en"
+                }
+            ]
+        }
+    ]
+}
+
+headers = {
+  'Ocp-Apim-Subscription-Key': subscriptionKey,
+  'Content-Type': 'application/json'
+}
+
+response = requests.post(constructed_url, headers=headers, json=payload)
+
+print(f'response status code: {response.status_code}\nresponse status: {response.reason}\nresponse headers: {response.headers}')
